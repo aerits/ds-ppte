11ecf3d88a6a329d917ea3d30223e4d6af80166c
aerits <stev_nm@protonmail.com>
2025-05-21 02:37:21 +0000

    Testing endless buyos game mode

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::mem::MaybeUninit;
 use std::ptr::{self, null, null_mut};
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
-use std::time::{Instant, SystemTime, UNIX_EPOCH};
+use std::thread::sleep;
+use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 
 use blockstackers_core::blockstacker::{self, BlockStacker, Tuning};
 use blockstackers_core::buyo_game::BuyoBuyo;
@@ -38,17 +39,17 @@ fn main() {
     };
 
     let color = unsafe { C2D_Color32(255, 255, 255, 0) };
-    let i: i8 = rng.random();
-    let game;
-    if i >= 0 {
-        game = "tet"
-    } else {
-        game = "buyo"
-    }
+    // let i: i8 = rng.random();
+    // let game;
+    // if i >= 0 {
+    //     game = "tet"
+    // } else {
+    //     game = "buyo"
+    // }
     let mut game: Box<dyn BlockStacker> = <dyn BlockStacker>::new(
-        game,
-        10,
-        24,
+        "buyo",
+        6,
+        12,
         Randomizer::new(
             7,
             SystemTime::now()
@@ -58,6 +59,7 @@ fn main() {
         ),
         Tuning::new(),
     );
+    let mut pieces_placed = 1;
     let time_start = Instant::now();
     let mut time_last_update = Instant::now();
 
@@ -103,6 +105,29 @@ fn main() {
         ) { 
             time_last_update = Instant::now();
             update = true;
+            if game.get_loop_state() == "SpawnNew" {
+                pieces_placed += 1;
+                
+            } else if game.get_loop_state() == "Dead" {
+                sleep(Duration::from_secs(5));
+                game = <dyn BlockStacker>::new(
+                    "buyo",
+                    6,
+                    12,
+                    Randomizer::new(
+                        7,
+                        SystemTime::now()
+                            .duration_since(UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis(),
+                    ),
+                    Tuning::new(),
+                );
+            }
+            let score = game.total_score();
+            let tuning = game.get_mut_tuning();
+            tuning.fall_speed = 100;
+            tuning.fall_skip = 0.2 + pieces_placed as f32 / 100.0;
         }
 
         if !update {
@@ -113,6 +138,8 @@ fn main() {
         unsafe {
             println!("\x1b[14;00H");
             println!("{}, {}", C3D_GetDrawingTime(), C3D_GetProcessingTime());
+            println!("level: {}", pieces_placed);
+            // println!("fall skip: {}", 0.2 + (game.total_score() as f32 / 100f32) % 10.0);
             C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
             C2D_TargetClear(render_target, color);
             C2D_SceneBegin(render_target);
@@ -122,22 +149,22 @@ fn main() {
             C2D_TextParse(
                 text.as_mut_ptr(),
                 buf,
-                (game.total_score().to_string() + "\0").as_ptr(),
+                (game.total_score().to_string() + "\nlevel: " + &pieces_placed.to_string() + "\0").as_ptr(),
             );
             C2D_TextOptimize(text.as_mut_ptr());
             C2D_DrawText(text.as_ptr(), 0, 120.0, 30.0, 0.0, 1.0, 1.0);
-
+            const RADIUS: f32 = 8.0;
             for (v, s) in game.get_board() {
                 let color = get_sprite(s);
-                C2D_DrawCircleSolid(v.x as f32 * 8f32, v.y as f32 * 8f32, 0f32, 4f32, color);
+                C2D_DrawCircleSolid(v.x as f32 * 2.0 * RADIUS, v.y as f32 * 2.0 * RADIUS, 0f32, RADIUS, color);
             }
             for (x, y, s) in game.get_controlled_block() {
                 let color = get_sprite(s);
-                C2D_DrawCircleSolid(x as f32 * 8f32, y as f32 * 8f32, 0f32, 4f32, color);
+                C2D_DrawCircleSolid(x as f32 * 2.0 * RADIUS, y as f32 * 2.0 * RADIUS, 0f32, RADIUS, color);
             }
             for (v, s) in game.next_queue() {
                 let color = get_sprite(s);
-                C2D_DrawCircleSolid(v.x as f32 * 8f32 + 200.0, v.y as f32 * 8f32 + 100.0, 0f32, 4f32, color);
+                C2D_DrawCircleSolid(v.x as f32 * 2.0 * RADIUS + 200.0, v.y as f32 * 2.0 * RADIUS + 100.0, 0f32, RADIUS, color);
             }
 
             // println!("{}", C3D_GetDrawingTime());
